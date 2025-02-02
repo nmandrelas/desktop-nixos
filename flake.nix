04d36d4af86a934d06632b7af9a691f2d1963a7a
nmandrelas <nmandrelas@gmail.com>
2025-02-02 23:17:32 +0200

    test

--- flake.nix
+++ flake.nix
@@ -1,21 +1,22 @@
 {
+  description = "Nixos config flake";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-    tuxedo-nixos = {
-      url = "github:blitz/tuxedo-nixos";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
-  outputs = { self, nixpkgs, tuxedo-nixos }: {
-    nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
-        modules = [
-          ./configuration.nix
-          tuxedo-nixos.nixosModules.default
-          { hardware.tuxedo-control-center.enable = true; }
-        ];
-      };
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+        # inputs.home-manager.nixosModules.default
+      ];
     };
   };
 }
