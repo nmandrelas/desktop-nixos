58eee49d5bbac987dc8e49a15e6086eeb8890b1f
nmandrelas <nmandrelas@gmail.com>
2025-02-04 11:04:38 +0200

    test

--- users/hypr/home.nix
+++ users/hypr/home.nix
@@ -96,10 +96,4 @@
     nvidiaPatches = true;
     xwayland.enable = true;
   };
-  environment.sessionVariables = {
-   #If your cursor becomes invisible
-   WLR_NO_HARDWARE_CURSORS = "1";
-   #Hint electron apps to use wayland
-   NIXOS_OZONE_WL = "1";
-  };
 }
