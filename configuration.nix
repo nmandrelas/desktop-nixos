05f4041683b1531c6814925131a34f3e014f2278
nmandrelas <nmandrelas@gmail.com>
2025-02-04 13:43:14 +0200

    starship

--- configuration.nix
+++ configuration.nix
@@ -58,9 +58,6 @@
     # Enable the GNOME Desktop Environment.
     displayManager = {
       gdm.enable = true;
-      # Enable automatic login for the user.
-      autoLogin.enable = true;
-      autoLogin.user = "makys";
     };
     desktopManager ={
       gnome.enable = true;
@@ -71,7 +68,11 @@
       variant = "";
     };
   };
-  
+  services.displayManager.autologin = {
+      # Enable automatic login for the user.
+      autoLogin.enable = true;
+      autoLogin.user = "makys";
+  };
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
@@ -175,55 +176,55 @@
     pkgs.starship
   ];
   # TODO: Configure starship correctly
-  programs.starship = {
-    enable = true;
-    settings = {
-      add_newline = true;
-      command_timeout = 1000;
-      format = "$username";
-      character = {
-        success_symbol = "[󰜃 ](bright-cyan)";
-        error_symbol = "[](red)";
-      };
-      username = {
-        style_user = "white";
-        style_root = "white";
-        format = "[$user]($style) ";
-        disabled = false;
-        show_always = true;
-      };
-      hostname = {
-        ssh_only = false;
-        format = "@ [$hostname](bold yellow) ";
-        disabled = false;
-      };
-      directory = {
-        home_symbol = "󰋞 ~";
-        read_only_style = "197";
-        read_only = "  ";
-        format = "at [$path]($style)[$read_only]($read_only_style) ";
-      };
-      git_branch = {
-        symbol = " ";
-        format = "via [$symbol$branch]($style) ";
-        style = "bold green";
-      };
-      git_status = {
-        format = "[\($all_status$ahead_behind\)]($style) ";
-        style = "bold green";
-        conflicted = "🏳";
-        up_to_date = " ";
-        untracked = " ";
-        ahead = "⇡\${count}";
-        diverged = "⇕⇡\${ahead_count}⇣\${behind_count}";
-        behind = "⇣\${count}";
-        stashed = " ";
-        modified = " ";
-        staged = "[++\($count\)](green)";
-        renamed = "襁 ";
-        deleted = " ";
-      };
-    };
-  };
+  # programs.starship = {
+  #   enable = true;
+  #   settings = {
+  #     add_newline = true;
+  #     command_timeout = 1000;
+  #     format = "$username";
+  #     character = {
+  #       success_symbol = "[󰜃 ](bright-cyan)";
+  #       error_symbol = "[](red)";
+  #     };
+  #     username = {
+  #       style_user = "white";
+  #       style_root = "white";
+  #       format = "[$user]($style) ";
+  #       disabled = false;
+  #       show_always = true;
+  #     };
+  #     hostname = {
+  #       ssh_only = false;
+  #       format = "@ [$hostname](bold yellow) ";
+  #       disabled = false;
+  #     };
+  #     directory = {
+  #       home_symbol = "󰋞 ~";
+  #       read_only_style = "197";
+  #       read_only = "  ";
+  #       format = "at [$path]($style)[$read_only]($read_only_style) ";
+  #     };
+  #     git_branch = {
+  #       symbol = " ";
+  #       format = "via [$symbol$branch]($style) ";
+  #       style = "bold green";
+  #     };
+  #     git_status = {
+  #       format = "[\($all_status$ahead_behind\)]($style) ";
+  #       style = "bold green";
+  #       conflicted = "🏳";
+  #       up_to_date = " ";
+  #       untracked = " ";
+  #       ahead = "⇡\${count}";
+  #       diverged = "⇕⇡\${ahead_count}⇣\${behind_count}";
+  #       behind = "⇣\${count}";
+  #       stashed = " ";
+  #       modified = " ";
+  #       staged = "[++\($count\)](green)";
+  #       renamed = "襁 ";
+  #       deleted = " ";
+  #     };
+  #   };
+  # };
 
 }
