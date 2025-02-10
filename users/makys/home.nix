e0bbae3c2dc26699b63a10163505559b1a5a98fe
nmandrelas <nmandrelas@gmail.com>
2025-02-10 12:07:05 +0200

    Fonts support

--- users/makys/home.nix
+++ users/makys/home.nix
@@ -139,19 +139,7 @@
     # cursorTheme.name = "macOS";
     # cursorTheme.size = 20;
     font.name = "MesloLGS Nerd Font";
-    gtk3.extraConfig = {
-      Settings = ''
-                				gtk-application-prefer-dark-theme=1
-        					      gtk-font-name=MesloLGS Nerd Font
-              				'';
-    };
-
-    gtk4.extraConfig = {
-      Settings = ''
-                				gtk-application-prefer-dark-theme=1
-        					      gkt-font-name=MesloLGS Nerd Font
-              				'';
-    };
+    font.size = 24;
   };
 
   # Let Home Manager install and manage itself.
