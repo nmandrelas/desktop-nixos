2aa59d5167a0bde1b20855ccee59f6ddf4352c9e
nmandrelas <nmandrelas@gmail.com>
2025-02-20 22:08:01 +0200

    Mono ;

--- system/games.nix
+++ system/games.nix
@@ -6,6 +6,9 @@
     bottles # Bottles introduces a new way to handle Windows prefixes using environments#
     heroic # heroic games launcher#
     prismlauncher # used to manage minecraft#
+    mono
+    mono4
+    mono5
   ];
   programs.steam = {
     enable = true;
