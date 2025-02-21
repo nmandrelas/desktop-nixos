942e4c32a1a3ccec1b4d0252436464ad2e1d84a3
nmandrelas <nmandrelas@gmail.com>
2025-02-21 18:35:58 +0200

    hyprland

--- users/makys/modules/wms/hyprpaper.nix
+++ users/makys/modules/wms/hyprpaper.nix
@@ -5,8 +5,7 @@
       preload = [ "~/.config/backgrounds/my_bgs/nix-wallpaper-binary-green.png" ];
 
       wallpapers = [
-        "DP-1, ~/.config/backgrounds/my_bgs/nix-wallpaper-binary-green.png"
-        "DP-3, ~/.config/backgrounds/my_bgs/nix-wallpaper-binary-green.png"
+        ", ~/.config/backgrounds/my_bgs/nix-wallpaper-binary-green.png"
       ];
     };
   };
