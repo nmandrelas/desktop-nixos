29446190074fe2f2765f487021cbfc106f54be02
nmandrelas <nmandrelas@gmail.com>
2025-02-21 09:46:30 +0200

    Test

--- flake.nix
+++ flake.nix
@@ -3,33 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    catppuccin.url = "github:catppuccin/nix";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, catppuccin, home-manager, ... }@inputs: {
-    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: 
+    let
       system = "x86_64-linux";
+    in {
+
+    # nixos - system hostname
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       modules = [
         ./configuration.nix
-        catppuccin.nixosModules.catppuccin
-        inputs.home-manager.nixosModules.default
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.backupFileExtension = "home-manager-backup";
-          home-manager.users.makys = {
-            imports = [
-              ./users/makys/home.nix
-              catppuccin.homeManagerModules.catppuccin
-            ];
-          };
-          # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
-        }
       ];
     };
-  };
+
+    homeConfigurations.makys = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.${system};
+      modules = [ ./users/makys/home.nix ];
+    };
+  }
 }
