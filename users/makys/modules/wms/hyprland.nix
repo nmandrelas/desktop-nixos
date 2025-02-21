710352aa4002a2d5e64d57937f0fbbdea1fd9bcd
nmandrelas <nmandrelas@gmail.com>
2025-02-21 18:12:54 +0200

    hyprland

---
+++ users/makys/modules/wms/hyprland.nix
@@ -0,0 +1,220 @@
+{
+  wayland.windowManager.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+
+    settings = {
+      "$mainMod" = "SUPER";
+      "$terminal" = "kitty";
+      "$fileManager" = "nautilus";
+      "$menu" = "wofi --show drun --allow-images --insensitive";
+
+      monitor = [
+       "DP-3, 1920x1080x60.00Hz,0x0, 1"
+       "DP-1, 2560x1440@164.96Hz, 1920x0, 1"
+      ];
+
+      env = [
+        "XCURSOR_SIZE,24"
+        "HYPRCURSOR_SIZE,24"
+        "HYPRCURSOR_THEME,nordzy"
+        "ELECTRON_OZONE_PLATFORM_HINT,1"
+      ];
+
+
+      render = {
+        explicit_sync = 2;
+        explicit_sync_kms = 0;
+      };
+      opengl = {
+        nvidia_anti_flicker = 0;
+        force_introspection = 2;
+      };
+
+      misc = {
+        vfr = 0;
+      };
+
+      debug = {
+        disable_logs = false;
+        enable_stdout_logs = true;
+        damage_tracking = 0;
+      };
+
+      input = {
+        kb_layout = "us, gr";
+        kb_variant = "lang";
+        kb_options = "grp:alt_space_toggle";
+
+        follow_mouse = 1;
+
+        touchpad = {
+          natural_scroll = false;
+        };
+
+        sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
+      };
+
+      general = {
+        gaps_in = 5;
+        gaps_out = 20;
+        border_size = 3;
+        "col.active_border" = "rgba(21B16Bee) rgba(66CDAAee) 45deg";
+        "col.inactive_border" = "rgba(11111baa)";
+        resize_on_border = true;
+        layout= "dwindle";
+        allow_tearing = false;
+        no_cursor_warps = false;
+      };
+
+      decoration = {
+        rounding = 10;
+        active_opacity = 1.0;
+        inactive_opacity = 1.0;
+
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 1;
+          new_optimizations = true;
+          vibrancy = 0.1696;
+        };
+
+        drop_shadow = true;
+        shadow_range = 4;
+        shadow_render_power = 3;
+        "col.shadow" = "rgba(11111bee)";
+      };
+
+      animations = {
+        enabled = true;
+
+        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+        # bezier = "myBezier, 0.33, 0.82, 0.9, -0.08";
+
+        animation = [
+          "windows,     1, 7,  myBezier"
+          "windowsOut,  1, 7,  default, popin 80%"
+          "border,      1, 10, default"
+          "borderangle, 1, 8,  default"
+          "fade,        1, 7,  default"
+          "workspaces,  1, 6,  default"
+        ];
+      };
+
+      dwindle = {
+        pseudotile = true; # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+        preserve_split = true; # you probably want this
+      };
+
+      master = {
+        new_is_master = true;
+      };
+
+      gestures = {
+        workspace_swipe = false;
+      };
+
+      misc = {
+        animate_manual_resizes = true;
+        animate_mouse_windowdragging = true;
+        enable_swallow = true;
+        render_ahead_of_time = false;
+        disable_hyprland_logo = false;
+      };
+
+      windowrule = [
+        "tile, ^(kitty)$"
+      ];
+      windowrulev2 = [
+        "suppressevent maximize, class:.*"
+        "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+      ];
+
+      exec-once = [
+        "waybar & hyprpaper & swaync"
+        "hyprctl setcursor nordzy-latte-green 24"
+      ];
+
+      bind = [
+        "$mainMod, V, exec, cliphist list | wofi --dmenu | cliphist decode | wl-copy"
+
+        "$mainMod, Q, exec, $terminal"
+        "$mainMod, C, killactive,"
+        "$mainMod, M, exit,"
+        "$mainMod, E, exec, $fileManager"
+        "$mainMod, V, togglefloating,"
+        "$mainMod, R, exec, $menu"
+        "$mainMod, P, pseudo, # dwindle"
+        "$mainMod, J, togglesplit, # dwindle"
+
+        # Move focus with mainMod + arrow keys
+        "$mainMod, left,  movefocus, l"
+        "$mainMod, right, movefocus, r"
+        "$mainMod, up,    movefocus, u"
+        "$mainMod, down,  movefocus, d"
+
+        # Moving windows
+        "$mainMod SHIFT, left,  swapwindow, l"
+        "$mainMod SHIFT, right, swapwindow, r"
+        "$mainMod SHIFT, up,    swapwindow, u"
+        "$mainMod SHIFT, down,  swapwindow, d"
+
+        # Window resizing                     X  Y
+        "$mainMod CTRL, left,  resizeactive, -60 0"
+        "$mainMod CTRL, right, resizeactive,  60 0"
+        "$mainMod CTRL, up,    resizeactive,  0 -60"
+        "$mainMod CTRL, down,  resizeactive,  0  60"
+
+        # Switch workspaces with mainMod + [0-9]
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+        "$mainMod, 6, workspace, 6"
+        "$mainMod, 7, workspace, 7"
+        "$mainMod, 8, workspace, 8"
+        "$mainMod, 9, workspace, 9"
+        "$mainMod, 0, workspace, 10"
+
+        # Move active window to a workspace with mainMod + SHIFT + [0-9]
+        "$mainMod SHIFT, 1, movetoworkspacesilent, 1"
+        "$mainMod SHIFT, 2, movetoworkspacesilent, 2"
+        "$mainMod SHIFT, 3, movetoworkspacesilent, 3"
+        "$mainMod SHIFT, 4, movetoworkspacesilent, 4"
+        "$mainMod SHIFT, 5, movetoworkspacesilent, 5"
+        "$mainMod SHIFT, 6, movetoworkspacesilent, 6"
+        "$mainMod SHIFT, 7, movetoworkspacesilent, 7"
+        "$mainMod SHIFT, 8, movetoworkspacesilent, 8"
+        "$mainMod SHIFT, 9, movetoworkspacesilent, 9"
+        "$mainMod SHIFT, 0, movetoworkspacesilent, 10"
+
+        # Example special workspace (scratchpad)
+        "$mainMod, S, togglespecialworkspace, magic"
+        "$mainMod SHIFT, S, movetoworkspace, special:magic"
+        
+        # Scroll through existing workspaces with mainMod + scroll
+        "$mainMod, mouse_down, workspace, e+1"
+        "$mainMod, mouse_up, workspace, e-1"
+
+        # Waybar
+        "$mainMod, B, exec, pkill -SIGUSR1 waybar"
+        "$mainMod, W, exec, pkill -SIGUSR2 waybar"
+
+        # custom
+        "$mainMod, SPACE, exec, $menu"
+        ", PRINT, exec, hyprshot -m region"
+        "SHIFT , PRINT, exec, hyprshot -m window"
+        " $SUPER_SHIFT, l, exec, hyprlock"
+        "$mainMod, F, fullscreen,"
+      ];
+
+      # Move/resize windows with mainMod + LMB/RMB and dragging
+      bindm = [
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
+      ];
+    };
+  };
+}
