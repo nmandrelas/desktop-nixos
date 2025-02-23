aab19066917aab1e5438119e680b98bcbfa4152c
nmandrelas <nmandrelas@gmail.com>
2025-02-23 12:03:03 +0200

    fonts

--- system/packages.nix
+++ system/packages.nix
@@ -1,11 +1,7 @@
 { pkgs, pkgs-unstable, ... }: {
-    nixpkgs.config = {
-      allowUnfree = true;
-      permittedInsecurePackages = [
-        "dotnet-runtime-7.0.20"
-      ];
-    };
-
+    nixpkgs.config.permittedInsecurePackages = [
+      "dotnet-runtime-7.0.20"
+    ];
     fonts.packages = with pkgs; [
       noto-fonts
       noto-fonts-cjk-sans
