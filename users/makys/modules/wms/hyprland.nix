805338bcc35cdbfce4f541c9812c34efb1b1a600
nmandrelas <nmandrelas@gmail.com>
2025-03-03 00:15:28 +0200

    hyprland

--- users/makys/modules/wms/hyprland.nix
+++ users/makys/modules/wms/hyprland.nix
@@ -247,8 +247,8 @@
         "SHIFT , PRINT, exec, hyprshot -m window"
         " $SUPER_SHIFT, l, exec, hyprlock"
         "$mainMod, F, fullscreen,"
-        #"ALT, TAB, workspace, -1"
-        #"ALT AND SHIFT, TAB, workspace, +1"
+        "ALT, A, workspace, -1"
+        "ALT, D, workspace, +1"
       ];
 
       # Move/resize windows with mainMod + LMB/RMB and dragging
