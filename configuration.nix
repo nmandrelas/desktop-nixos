6a051443761fc9544018643f76e0b64254bf4850
nmandrelas <nmandrelas@gmail.com>
2025-04-05 13:22:39 +0300

    disable ipv6

--- configuration.nix
+++ configuration.nix
@@ -142,6 +142,7 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+  networking.enableIPv6 = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
