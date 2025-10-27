b4b2c3b032f0886ee38eeeff31261f8e6cca451b
nmandrelas <nmandrelas@gmail.com>
2025-10-27 11:39:34 +0200

    first try

--- system/work.nix
+++ system/work.nix
@@ -1,7 +1,7 @@
 { config, pkgs, pkgs-unstable, ... }: {
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages 
+  environment.systemPackages =
     [
       pkgs-unstable.citrix_workspace # the devil#
     ];
