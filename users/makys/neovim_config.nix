0293f32d531cc8d31796015e9c74d4e9ec23e376
nmandrelas <nmandrelas@gmail.com>
2025-12-17 23:51:40 +0200

    neovim

--- users/makys/neovim_config.nix
+++ users/makys/neovim_config.nix
@@ -6,50 +6,57 @@
     theme.enable = true;
     theme.name = "catppuccin";
     theme.style = "mocha";
-    telescope = { 
-      enable = true; 
-      };
+    telescope = { enable = true; };
     globals.mapleader = " ";
     # You can also enable languages, Treesitter, LSP, etc.
     # vim.languages.nix.enable = true;
     treesitter.enable = true;
     binds.whichKey.enable = true;
     utility.undotree.enable = true;
-    keymaps = [{
-      key = "<leader>u";
-      mode = "n";
-      silent = true;
-      action = ":UndotreeToggle<CR>";
-      desc = "show undotree";
-    }
-    {
-      key = "<C-d>";
-      mode = "n";
-      silent = true;
-      action = "<C-d>zz";
-      desc = "Scroll down and center";
-    }
-  {
-      key = "<C-u>";
-      mode = "n";
-      silent = true;
-      action = "<C-u>zz";
-      desc = "Scroll up and center";
-    }
-    {
-      key = "n";
-      mode = "n";
-      silent = true;
-      action = "nzzzv";
-      desc = "Find and center";
-    }
-    {
-      key = "N";
-      mode = "n";
-      silent = true;
-      action = "Nzzzv";
-      desc = "Find and center";
-    }
+    keymaps = [
+      {
+        key = "<leader>u";
+        mode = "n";
+        silent = true;
+        action = ":UndotreeToggle<CR>";
+        desc = "show undotree";
+      }
+      {
+        key = "<C-d>";
+        mode = "n";
+        silent = true;
+        action = "<C-d>zz";
+        desc = "Scroll down and center";
+      }
+      {
+        key = "<C-u>";
+        mode = "n";
+        silent = true;
+        action = "<C-u>zz";
+        desc = "Scroll up and center";
+      }
+      {
+        key = "n";
+        mode = "n";
+        silent = true;
+        action = "nzzzv";
+        desc = "Find and center";
+      }
+      {
+        key = "N";
+        mode = "n";
+        silent = true;
+        action = "Nzzzv";
+        desc = "Find and center";
+      }
+      {
+        key = "<leader>p";
+        mode = "x";
+        silent = true;
+        action = ''"_dP'';
+        desc = "Paste without overwriting clipboard";
+      }
+
     ];
 
   };
