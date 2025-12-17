44002a0976707472d9c2ad644f1653c9c450791f
nmandrelas <nmandrelas@gmail.com>
2025-12-17 23:22:51 +0200

    neovim

--- users/makys/neovim_config.nix
+++ users/makys/neovim_config.nix
@@ -1,14 +1,15 @@
 { pkgs, lib, ... }:
 
 {
-  vim = {
-    theme.enable = true;
-    theme.name = "catppuccin";
-    theme.style = "mocha";
-    # add more NVF options here as needed
+  theme.enable = true;
+  theme.name = "catppuccin";
+  theme.style = "mocha";
+  telescope = {
+    enable = true;
   };
+  globals.mapleader = " ";
 
   # You can also enable languages, Treesitter, LSP, etc.
   # vim.languages.nix.enable = true;
-  # vim.treesitter.enable = true;
+  vim.treesitter.enable = true;
 }
