206e2a92a39d472f5d6a334eecfce1a97fa38761
nmandrelas <nmandrelas@gmail.com>
2025-12-23 01:57:41 +0200

    trying to merge laptot

--- system/work.nix
+++ system/work.nix
@@ -2,7 +2,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages =
-    [
-      nixpkgs-24-11.citrix_workspace # the devil#
-    ];
+  [
+    nixpkgs-24-11.citrix_workspace # the devil#
+  ];
+  environment.systemPackages = [ (citrix_workspace.override {}) ];
 }
