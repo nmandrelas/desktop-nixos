2510976d2c699cdf7b2b345914772d8375caa376
nmandrelas <nmandrelas@gmail.com>
2026-03-03 10:29:53 +0200

    docker cleanup

--- system/docker.nix
+++ system/docker.nix
@@ -8,14 +8,15 @@
       setSocketVariable = true;
     };
     daemon.settings = {
-    default-runtime = "nvidia";
-    runtimes = {
-      nvidia = {
-        path = "nvidia-container-runtime";
-        runtimeArgs = [];
+      default-runtime = "nvidia";
+      runtimes = {
+        nvidia = {
+          path = "nvidia-container-runtime";
+          runtimeArgs = [ ];
+        };
       };
     };
   };
-    
+
   virtualisation.docker.daemon.settings = { data-root = "~/docker"; };
 }
