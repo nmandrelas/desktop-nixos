c8e46bf5ff72f8c9ee81a984ef310920a4de1e79
nmandrelas <nmandrelas@gmail.com>
2026-03-03 10:29:11 +0200

    docker cleanup

--- system/docker.nix
+++ system/docker.nix
@@ -1,10 +1,21 @@
 { config, pkgs, ... }: {
   # Docker
-  virtualisation.docker.enable = true;
-  virtualisation.docker.enableNvidia = true;
-  virtualisation.docker.rootless = {
+  virtualisation.docker = {
     enable = true;
-    setSocketVariable = true;
+    enableNvidia = true;
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
+    daemon.settings = {
+    default-runtime = "nvidia";
+    runtimes = {
+      nvidia = {
+        path = "nvidia-container-runtime";
+        runtimeArgs = [];
+      };
+    };
   };
+    
   virtualisation.docker.daemon.settings = { data-root = "~/docker"; };
 }
