6265c41ad5f90eedbded8cda385c5bc750ed65f9
nmandrelas <nmandrelas@gmail.com>
2026-03-12 11:35:39 +0200

    Fonts for steam updated

--- system/games.nix
+++ system/games.nix
@@ -11,6 +11,13 @@
     enable = true;
     dedicatedServer.openFirewall = true;
     gamescopeSession.enable = true;
+    fontPackages = with pkgs; [
+      corefonts
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      liberation_ttf
+    ];
   };
   programs.gamemode.enable = true;
 }
