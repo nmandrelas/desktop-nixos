3f7675f61fdd8213eff4b56bf387cdf6b5f97f82
nmandrelas <nmandrelas@gmail.com>
2026-03-25 15:50:36 +0200

    disable runtime, enable cdi

--- system/docker.nix
+++ system/docker.nix
@@ -9,12 +9,13 @@
     };
     daemon.settings = {
       default-runtime = "nvidia";
-      runtimes = {
-        nvidia = {
-          path = "nvidia-container-runtime";
-          runtimeArgs = [ ];
-        };
-      };
+      cdi = true;
+      # runtimes = {
+      #   nvidia = {
+      #     path = "nvidia-container-runtime";
+      #     runtimeArgs = [ ];
+      #   };
+      # };
     };
   };
   virtualisation.docker.daemon.settings = { data-root = "~/docker"; };
