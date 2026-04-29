82135051748ea2a27fff9b6675da810e4ddca17f
nmandrelas <nmandrelas@gmail.com>
2026-04-29 10:16:57 +0300

    neotree config

--- nixvim.nix
+++ nixvim.nix
@@ -260,8 +260,25 @@
     # ---------------------
     # File explorer
     # ---------------------
-    neo-tree.enable = true;
+    neo-tree = {
+      enable = true;
+      settings = {
+        close_if_last_window = true;
+        filesystem = {
+          follow_current_file = {
+              enabled = true;
+              leave_dirs_open = true;
+          };
+          filtered_items = {
+            visible = true;
+            hide_dotfiles = false;
+            hide_gitignored = true;
+          };
+        };
+      };
+    };
 
+    };
     # ---------------------
     # Bufferline (tabs)
     # ---------------------
