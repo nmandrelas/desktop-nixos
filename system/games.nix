1873d70af2ff5acd4033e33a0595e5a54e46f59e
nmandrelas <nmandrelas@gmail.com>
2026-05-18 10:02:19 +0300

    heroic

--- system/games.nix
+++ system/games.nix
@@ -4,7 +4,7 @@
     protonup-qt # Install and manage Proton-GE for Steam and Wine-GE for Lutris with this graphical user interface#
     #lutris # Lutris is a video game preservation platform#
     #bottles # Bottles introduces a new way to handle Windows prefixes using environments#
-    #heroic # heroic games launcher#
+    heroic # heroic games launcher#
     prismlauncher # used to manage minecraft#
   ];
   programs.steam = {
