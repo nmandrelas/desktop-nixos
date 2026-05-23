971b99aba24ac00b8206372fa63daebd2df06c4a
nmandrelas <nmandrelas@gmail.com>
2026-05-23 11:37:52 +0300

    waybar llm

--- users/makys/modules/packages.nix
+++ users/makys/modules/packages.nix
@@ -8,10 +8,10 @@
     (pkgs.writeShellScriptBin "audio-switcher" ''
       action=$1
       if [ "$action" = "sink" ]; then
-        devices=$(wpctl status | awk '/Sinks:/{f=1;next} /Sources:/{f=0} f' | grep -E "[0-9]+\." | sed -E 's/.*│[ \*]+([0-9]+)\. (.*) \[vol:.*/\1: \2/')
+        devices=$(wpctl status | awk '/Sinks:/{f=1;next} /Sources:/{f=0} f' | grep -E "[0-9]+\." | sed -E -e 's/.*│ *\* *([0-9]+)\. (.*) \[vol:.*/\1: 🟢 \2/' -e 's/.*│ *([0-9]+)\. (.*) \[vol:.*/\1: \2/')
         prompt="Select Output Device"
       elif [ "$action" = "source" ]; then
-        devices=$(wpctl status | awk '/Video/{exit} /Sources:/{f=1;next} /Filters:/{f=0} f' | grep -E "[0-9]+\." | sed -E 's/.*│[ \*]+([0-9]+)\. (.*) \[vol:.*/\1: \2/')
+        devices=$(wpctl status | awk '/Video/{exit} /Sources:/{f=1;next} /Filters:/{f=0} f' | grep -E "[0-9]+\." | sed -E -e 's/.*│ *\* *([0-9]+)\. (.*) \[vol:.*/\1: 🟢 \2/' -e 's/.*│ *([0-9]+)\. (.*) \[vol:.*/\1: \2/')
         prompt="Select Input Device"
       else
         exit 1
