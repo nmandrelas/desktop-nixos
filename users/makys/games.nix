33cc1922850ec12d475785af8713c49a7536af7d
nmandrelas <nmandrelas@gmail.com>
2026-05-24 10:56:44 +0300

    moved to home

--- users/makys/games.nix
+++ users/makys/games.nix
@@ -1,6 +1,13 @@
 { pkgs, pkgs-unstable, ... }: {
   home.packages = [
     pkgs-unstable.vintagestory
+    pkgs.mangohud # A Vulkan and OpenGL overlay for monitoring FPS#
+    pkgs.protonup-qt # Install and manage Proton-GE for Steam and Wine-GE for Lutris with this graphical user interface#
+    pkgs-unstable.lutris # Lutris is a video game preservation platform#
+    #bottles # Bottles introduces a new way to handle Windows prefixes using environments#
+    pkgs-unstable.heroic # heroic games launcher#
+    pkgs-unstable.prismlauncher # used to manage minecraft#
+
   ];
   programs.steam = {
     enable = true;
@@ -16,13 +23,4 @@
   };
   programs.gamemode.enable = true;
   programs.gamescope.enable = true;
-  home.packages = with pkgs; [
-    mangohud # A Vulkan and OpenGL overlay for monitoring FPS#
-    protonup-qt # Install and manage Proton-GE for Steam and Wine-GE for Lutris with this graphical user interface#
-    pkgs-unstable.lutris # Lutris is a video game preservation platform#
-    #bottles # Bottles introduces a new way to handle Windows prefixes using environments#
-    pkgs-unstable.heroic # heroic games launcher#
-    pkgs-unstable.prismlauncher # used to manage minecraft#
-  ];
-
 }
