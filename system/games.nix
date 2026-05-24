5bedc23ea02498b6955ef01df0bc78cbfe79315e
nmandrelas <nmandrelas@gmail.com>
2026-05-24 13:05:22 +0300

    unzip

--- system/games.nix
+++ system/games.nix
@@ -10,6 +10,12 @@
       noto-fonts-cjk-serif
       liberation_ttf
     ];
+    package = pkgs.steam.override {
+      extraPkgs = pkgs: with pkgs; [
+        unzip
+        p7zip # Good to have for other mods
+      ];
+   };
   };
   programs.gamemode.enable = true;
   programs.gamescope.enable = true;
