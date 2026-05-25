d91e85730d3d08249ecd1fb1244c59294a98dd32
nmandrelas <nmandrelas@gmail.com>
2026-05-25 11:39:52 +0300

    zsh & kitty update

--- users/makys/modules/terminal.nix
+++ users/makys/modules/terminal.nix
@@ -26,8 +26,36 @@
     enable = true;
     settings = {
       add_newline = false;
+      format = ''[\[](bold green)$username[@](bold green)$hostname[:](bold white)$directory[\]](bold green) $git_branch$character'';
+      
+      username = {
+        show_always = true;
+        format = ''[$user]($style)'';
+        style_user = "bold green";
+      };
+
+      hostname = {
+        ssh_only = false;
+        format = ''[$hostname]($style)'';
+        style_info = "bold green";
+      };
+
+      directory = {
+        format = ''[$path]($style)'';
+        truncation_length = 0;
+        truncate_to_repo = false;
+        style = "bold blue";
+      };
+
       git_branch = {
-        symbol = "🌱 ";
+        symbol = "#";
+        format = ''[$symbol$branch]($style) '';
+        style = "bold purple";
+      };
+
+      character = {
+        success_symbol = ''[\$](bold white) '';
+        error_symbol = ''[\$](bold red) '';
       };
     };
   };
